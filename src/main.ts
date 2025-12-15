f6428b834f4572ffa99204e1d0530548ec335a87
Carlos Gonzalez <carlossgv@gmail.com>
2025-12-15 20:17:08 -0300

    fix: timezone error

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
@@ -5,8 +8,11 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); // optional
-  app.enableCors()
+  app.enableCors();
   await app.listen(3000);
   Logger.log(`Application is running on: ${await app.getUrl()}`);
+  console.log('env TZ:', process.env.TZ);
+  console.log('resolved tz:', Intl.DateTimeFormat().resolvedOptions().timeZone);
+  console.log('date:', new Date().toString());
 }
 void bootstrap();
